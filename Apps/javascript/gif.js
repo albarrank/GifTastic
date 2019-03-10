3da418156cb5b274617767cbad1c16df8a3b7afe
albarrank <albarrankevin97@gmail.com>
2019-03-09 23:06:06 -0800

    final changes

--- Apps/javascript/gif.js
+++ Apps/javascript/gif.js
@@ -20,110 +20,157 @@ $("document").ready(function() {
 
 
         var text = $("#newButton").val();
-        // console.log(text);
 
         topics.push(text);
 
         var tracker = topics.length;
         tracker--;
 
-        // console.log(topics);
-
-        // console.log("Array lenght when the text is added " + topics.length);
-
-        // console.log("New length when new text is added ; this returns its position  " + tracker);
+       
         
         for (var j = tracker; j < topics.length; j++) {
             var newButton = "<button class='button'>" + topics[tracker] + "</button>";
             $("#buttons").append(newButton);
-            // console.log(newButton);
         }
 
         $("#newButton").val(" ");
 
     });
 
-   
-    // console.log("Initial start length of array " + topics.length);
-    
+       
     
     //BUTTONS PRODUCE GIPHS 
+
     $(document).on("click", ".button", function() {
-        // console.log("hello");
 
         $("#imageWrapper").empty();
         
-        var searchText = this.innerHTML;
-        // console.log(searchText);
+        var text = this.innerHTML;
+        return ajaxResponse(text);
 
-
-
-         //AJAX IS GOING HERE (MAYBE PUT THIS IN A FUNCTION THAT RETURNS WHAT I WANT)
+    });
+    
+// --------------------------------------------------------------------------------------------------      
+    function ajaxResponse(searchText) {
+        //AJAX IS GOING HERE (MAYBE PUT THIS IN A FUNCTION THAT RETURNS WHAT I WANT)
 
         var search = searchText;
 
         var limit = 5;
-
+ 
         var APIKey = "iJg7SL7dckwMoOl3NPOeJ2Hy9pcDrpvw";
-
+ 
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=" + APIKey + "&limit=" + limit;
-
+ 
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response) {
-            console.log(response);
-            for (j = 0; j < limit; j++) {
-                var divTag = $("<div class='contentWrapper'>");
 
-                var rating = response.data[j].rating;
-                console.log(rating);
-                var headTag = $("<h3>");
-                headTag.html(rating);
-                console.log(headTag);
+            var gifArray =response.data;
+            return getAjaxData(gifArray); 
+        });
+// -----------------------------------------------------------------------//
+        function getAjaxData (gifArray) {
+            var imageArray = [];
 
-                // $("#imageWrapper").append("<h3 class='rating'>" + rating + "</h3>");
-
-                // NEED TO ADD ==> movingGif = response.data[j].images.orignal.url
-
-                var imageUrl = response.data[j].images.original_still.url;
-                var imageTag = $("<img>");
-                imageTag.attr("src", imageUrl);
-                console.log(imageTag);
-
-                divTag.prepend(headTag);
-                divTag.append(imageTag);
-
-                $("#imageWrapper").append(divTag);
-
-                // $("#imageWrapper").append("<img class='img' src=" + imageUrl + ">");
-                console.log(imageUrl);
-                
+            for (var i = 0; i < gifArray.length; i++) {
+                imageArray.push(gifArray[i]);
             }
+            showGifs(imageArray);
+            changeGifs(imageArray);
             
+        }
+        
+    }
+
+// --------------------------------------------------------------------------------------------    
+    function showGifs(imageArray) {
+        
+        limit = 2;
+
+        for (var j = 0; j < limit ; j++) {
+            var divTag = $("<div class='contentWrapper'>");
+            var rating =   imageArray[j].rating;
+            var headTag = $("<h3>");
+            headTag.html(rating);
+            var imageUrl = imageArray[j].images.original_still.url;
+            var imageTag = $("<img class='image'>");
+            divTag.prepend(headTag);
+            divTag.append(imageTag);
+            
+            if (imageUrl !== imageArray[j].images.original_still.url) {
+                imageUrl = imageArray[j].images.original_still.url;
+                imageTag.attr("src", imageUrl);
+            }
+            else if (imageUrl !== imageArray[j].images.original.url) {
+                imageUrl = imageArray[j].images.original.url;
+                imageTag.attr("src", imageUrl);
+            }
+
+            $("#imageWrapper").append(divTag);
+
+            
+ 
+        }
+
+    }
+
+    //   THIS WAS ESSENTIALLY GOING TO CHANGE THE SOURCE VALUE OF THE IMAGE
+
+    function changeGifs(imageArray) {
+        var y = imageArray;
+    /* HERE I WAS TRYING TO SET AN ARRAY OF THE ANIMATED IMAGE URL'S TO
+        SEE IF IT DIDNT MATCH BY THE STILL ONE PERTAINING TO WHAT
+        INDEX POSITION IT WAS IN */
+        $(document).on("click", ".image", function() {
+            limit = 5;
+            compareArray = y;
+            var urlArray =[];
+            console.log(compareArray);
+            for(k = 0; k < compareArray.length; k++) {
+                urlArray.push(compareArray[k].images.original.url);
+            }
+            console.log(urlArray);
+            thisImage = this;
+            for(l = 0; l < urlArray.length; l++) {
+                if ($(this).attr("src") !== urlArray[k]) {
+                    console.log("hello");
+                }
+            }
+            console.log(thisImage);            
+
         });
 
-        
-        
-    });
+    }
+
+    // function movingGif(animateGif) {
+    //     console.log(animateGif);
+    // }
+
     
-      
+    // function passArray(imageArray) {
+    //     console.log(imageArray);
+    // }
+    
+
 });
 
 
 
-var result = ('add', 4, 6);
 
-function calculator (operator, x, y) {
-    if(operator === 'add') {
-        var sum = add(x, y);
-        return sum;
-    }
-}
+// var result = ('add', 4, 6);
 
-function add (x, y) {
-    return x + y;
-}
+// function calculator (operator, x, y) {
+//     if(operator === 'add') {
+//         var sum = add(x, y);
+//         return sum;
+//     }
+// }
+
+// function add (x, y) {
+//     return x + y;
+// }
 
 
 
--- README.md
+++ README.md
@@ -3,3 +3,31 @@
 # This web app goes into giphy's server and returns values set by the parameters of the user.
 
 #This helps test you the use of AJAX and the API key.
+for (j = 0; j < limit; j++) {
+                 var divTag = $("<div class='contentWrapper'>");
+ 
+                 var rating = response.data[j].rating;
+                 console.log(rating);
+ 
+                 var headTag = $("<h3>");
+                 headTag.html(rating);
+                 console.log(headTag);
+ 
+                 // $("#imageWrapper").append("<h3 class='rating'>" + rating + "</h3>");
+ 
+                 // NEED TO ADD ==> movingGif = response.data[j].images.orignal.url
+ 
+                 var imageUrl = response.data[j].images.original_still.url;
+                 var imageTag = $("<img>");
+                 imageTag.attr("src", imageUrl);
+                 console.log(imageTag);
+ 
+                 divTag.prepend(headTag);
+                 divTag.append(imageTag);
+ 
+                 $("#imageWrapper").append(divTag);
+ 
+                 // $("#imageWrapper").append("<img class='img' src=" + imageUrl + ">");
+                 console.log(imageUrl);
+                 
+             }
